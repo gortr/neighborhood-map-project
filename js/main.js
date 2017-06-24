039fc1a1039478da8f1b22d3b2fc10e0a36ed3d3
Rigoberto J Gort <gort@bambinoscorner.com>
2017-06-23 22:42:56 -0400

    Implementing Filter Function (still not working)

--- js/main.js
+++ js/main.js
@@ -1,107 +1,6 @@
 // Makes sure code within this script is utilized as intended.
 'use strict';
 
-// --- GAMING LOCATIONS ---
-// Array of location markers.
-var locations = [
-	{title: 'Games Workshop', location: {lat: 34.823393, lng: -82.284911}},
-	{title: 'Cornermagic Gaming Center', location: {lat: 34.770675, lng: -82.46131}},
-	{title: 'Boardwalk', location: {lat: 34.84954, lng: -82.3297}},
-	{title: 'Borderlands', location: {lat: 34.843697, lng: -82.364306}},
-	{title: 'NextGen Trading', location: {lat: 34.784927, lng: -82.313561}},
-	{title: 'Video Game Cavern', location: {lat: 34.908524, lng: -82.327027}}
-];
-
-// --- MAIN SCRIPT ---
-// Global Declarations
-var map;
-var markers = [];
-
-// Populates markers with info.
-function populateInfoWindow(marker, infowindow) {
-	// Check to make sure infowindow is not already opened on this marker.
-	if (infowindow.marker != marker) {
-		// Clears infowindow
-		infowindow.setContent('');
-		infowindow.marker = marker;
-		infowindow.setContent('<div>' + marker.title + '</div>');
-		infowindow.open(map, marker);
-		marker.setAnimation(google.maps.Animation.BOUNCE);
-
-		setTimeout(function() {
-			marker.setAnimation(null);
-		}, 1400);
-
-		// Makes sure marker property is cleared if infowindow is closed.
-		infowindow.addListener('closeclick', function() {
-			infowindow.marker = null;
-		});
-		var streetViewService = new google.maps.StreetViewService();
-		var radius = 50;
-		// Calculates the panorama for the street view if all checks out.
-		function getStreetView(data, status) {
-			if (status == google.maps.StreetViewStatus.OK) {
-				var nearStreetViewLocation = data.location.latLng;
-				var heading = google.maps.geometry.spherical.computeHeading(
-					nearStreetViewLocation, marker.position);
-					infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-					var panoramaOptions = {
-						position: nearStreetViewLocation,
-						pov: {
-							heading: heading,
-							pitch: 15
-						}
-					};
-					var panorama = new google.maps.StreetViewPanorama(
-						document.getElementById('pano'), panoramaOptions);
-			} else {
-				infowindow.setContent('<div>' + marker.title + '</div>' +
-					'<div>No Street View Available</div>');
-			}
-		}
-		// Get closest streetview image within 50 meters of marker position.
-		streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-		// Open infowindow on correct marker.
-		infowindow.open(map, marker);
-	}
-}
-
-// Takes in a color and then creates a new marker icon based on the color scheme.
-function makeMarkerIcon(markerColor) {
-	var markerImage = new google.maps.MarkerImage(
-		'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-		'|40|_|%E2%80%A2',
-		new google.maps.Size(21, 34),
-		new google.maps.Point(0, 0),
-		new google.maps.Point(10, 34),
-		new google.maps.Size(21, 34));
-	return markerImage;
-}
-
-// Takes input from user and locates/zooms into specified area. Allows user to focus on one area of map.
-function zoomToArea() {
-	// Initialize GeoCoder
-	var geocoder = new google.maps.Geocoder();
-	// Get address or place that user inputted.
-	var address = document.getElementById('zoom-to-area-text').value;
-	// Make sure address isn't blank.
-	if (address == '') {
-		window.alert('You must enter an area or address');
-	} else {
-		// Geocode address/area inputted to get the center. Then center and zoom in on area.
-		geocoder.geocode(
-		{ 	address: address,
-		}, function(results, status) {
-			if (status == google.maps.GeocoderStatus.OK) {
-				map.setCenter(results[0].geometry.location);
-				map.setZoom(20);
-			} else {
-				window.alert('We could not find that location - please enter more specific details.');
-			}
-		});
-	}
-}
-
 // Styling for usage on map.
 var styles = [
     {
@@ -241,7 +140,137 @@ var styles = [
     }
 ];
 
-// --- MAP INITIALIZATION ---
+/* === GLOBAL DECLARATIONS === */
+var map;
+var markers = [];
+var self = this;
+
+// --- ARRAY OF GAMING LOCATIONS ---
+var locations = [
+	{title: 'Games Workshop', location: {lat: 34.823393, lng: -82.284911}},
+	{title: 'Cornermagic Gaming Center', location: {lat: 34.770675, lng: -82.46131}},
+	{title: 'Boardwalk', location: {lat: 34.84954, lng: -82.3297}},
+	{title: 'Borderlands', location: {lat: 34.843697, lng: -82.364306}},
+	{title: 'NextGen Trading', location: {lat: 34.784927, lng: -82.313561}},
+	{title: 'Video Game Cavern', location: {lat: 34.908524, lng: -82.327027}}
+];
+
+// Populates markers with info regarding location.
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+		// Clears infowindow
+		infowindow.setContent('');
+		infowindow.marker = marker;
+		infowindow.setContent('<div>' + marker.title + '</div>');
+		infowindow.open(map, marker);
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+
+		setTimeout(function() {
+			marker.setAnimation(null);
+		}, 1400);
+
+		// Makes sure marker property is cleared if infowindow is closed.
+		infowindow.addListener('closeclick', function() {
+			infowindow.marker = null;
+		});
+
+		/* === MARKER LOCATION STREETVIEW CODE === */
+		var streetViewService = new google.maps.StreetViewService();
+		var radius = 50;
+		// Calculates the panorama for the street view if all checks out.
+		function getStreetView(data, status) {
+			if (status == google.maps.StreetViewStatus.OK) {
+				var nearStreetViewLocation = data.location.latLng;
+				var heading = google.maps.geometry.spherical.computeHeading(
+					nearStreetViewLocation, marker.position);
+					infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+					var panoramaOptions = {
+						position: nearStreetViewLocation,
+						pov: {
+							heading: heading,
+							pitch: 15
+						}
+					};
+					var panorama = new google.maps.StreetViewPanorama(
+						document.getElementById('pano'), panoramaOptions);
+			} else {
+				infowindow.setContent('<div>' + marker.title + '</div>' +
+					'<div>No Street View Available</div>');
+			}
+		}
+		// Get closest streetview image within 50 meters of marker position.
+		streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+		// Open infowindow on correct marker.
+		infowindow.open(map, marker);
+	}
+}
+
+// Takes in a color and then creates a new marker icon based on the color scheme.
+function makeMarkerIcon(markerColor) {
+	var markerImage = new google.maps.MarkerImage(
+		'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
+		'|40|_|%E2%80%A2',
+		new google.maps.Size(21, 34),
+		new google.maps.Point(0, 0),
+		new google.maps.Point(10, 34),
+		new google.maps.Size(21, 34));
+	return markerImage;
+}
+
+// Takes input from user and locates/zooms into specified area. Allows user to focus on one area of map.
+function zoomToArea() {
+	// Initialize GeoCoder
+	var geocoder = new google.maps.Geocoder();
+	// Get address or place that user inputted.
+	var address = document.getElementById('zoom-to-area-text').value;
+	// Make sure address isn't blank.
+	if (address == '') {
+		window.alert('You must enter an area or address');
+	} else {
+		// Geocode address/area inputted to get the center. Then center and zoom in on area.
+		geocoder.geocode(
+		{ 	address: address,
+		}, function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK) {
+				map.setCenter(results[0].geometry.location);
+				map.setZoom(20);
+			} else {
+				window.alert('We could not find that location - please enter more specific details.');
+			}
+		});
+	}
+}
+
+// *************************
+// * - KO Data Structure - *
+// *************************
+var ViewModel = function() {
+	var self = this;
+
+	self.filterKeyword = ko.observable("");
+
+	self.locationsArray = ko.observableArray([]);
+
+	locations.forEach(function(locationItem) {
+		self.locationsArray.push(locations);
+	});
+
+	self.filteredLocations = ko.computed(function() {
+		var filter = this.filter().toLowerCase();
+		if (!filter) {
+			return this.items();
+		} else {
+			return ko.utils.arrayFilter(this.items(), function(item) {
+				return ko.utils.stringStartsWith(item.name().toLowerCase(), filter);
+			});
+		}
+	}, self);
+}
+
+// **************************
+// * - MAP INITIALIZATION - *
+// **************************
 function initMap() {
 
 	// Constructor creates a new map - only center and zoom are required.
@@ -251,12 +280,6 @@ function initMap() {
 		styles: styles,
 		mapTypeControl: false
 	});
-
-	// KO Data
-	var self = this;
-
-	// KO Behaviours
-
 
 	// This autocomplete is for use in geocoder entry box.
 	var zoomAutocomplete = new google.maps.places.Autocomplete(
@@ -270,7 +293,7 @@ function initMap() {
 	// This will be the default marker color.
 	var defaultIcon = makeMarkerIcon('0091FF');
 
-	// Iterates through the locations array.
+	// Iterates through the locations array and places the markers.
 	for (var i = 0; i < locations.length; i++) {
 		var position = locations[i].location;
 		var title = locations[i].title;
@@ -288,7 +311,7 @@ function initMap() {
 
 		// Create onClick event to open an infowindow for each marker.
 		marker.addListener('click', function() {
-			populateInfoWindow(this, largeInfowindow);
+			populateInfoWindow(self, largeInfowindow);
 		});
 
 		document.getElementById('zoom-to-area').addEventListener('click', function() {
@@ -297,14 +320,9 @@ function initMap() {
 
 		bounds.extend(markers[i].position);
 	}
-
 	map.fitBounds(bounds);
 }
 
-function launchMap() {
-	ko.applyBindings(new initMap());
-}
-
-function errorHandling() {
-	alert("Google Maps has to load per your request. Please verify your network connection shows internet as active.");
+function startMap() {
+	ko.applyBindings(new ViewModel());
 }
